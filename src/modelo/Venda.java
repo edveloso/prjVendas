0c580bf2d1f2430e04cf2dbb40431c587cbcd4c6
edveloso <edveloso@gmail.com>
2014-03-18 21:19:24 -0300

    initial commit

---
+++ src/modelo/Venda.java
@@ -0,0 +1,31 @@
+package modelo;
+
+public class Venda {
+
+	private int numero;
+	private double valorVendido;
+	private double vlDesconto;
+	private double valorVenda;
+	private Produto produto;
+	private Cliente cliente;
+	private Vendedor vendedor;
+	
+	public void efetuarVenda(Produto produto, int qtd, double desconto){
+		this.produto = produto;
+		this.vlDesconto = desconto;
+		//calcule o valor do valorVendido:(vlProduto-desconto)
+		//calcule o valor da venda:(valorVendido * qtd)
+		//gere uma nova venda: incrementando o numero da venda
+	}
+	
+	public void informarCliente ( Cliente cliente ){
+		this.cliente = cliente;
+	}
+	
+	public void informarVendedor( Vendedor vendedor ){
+		this.vendedor = vendedor;
+	}
+	
+	
+	
+}
---
+++ src/modelo/Vendedor.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Vendedor {
+
+	private String nome;
+	private int matricula;
+	private String endereco;
+	
+	//gets e sets
+	//clico em source -> generate gettets and setters
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	
+}
